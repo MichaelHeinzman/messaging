1bc2fc0eebdcf1f6fdf1aa9478fa2449db19c222
Seth Heinzman <sethheinzman@Seths-MacBook-Pro.local>
2023-05-24 16:59:07 -0500

    Updated messages to scroll to bottom when new message appears.

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,6 +1,6 @@
 import useAuth from "@/hooks/useAuth";
 import { useMessageSnapshot } from "@/hooks/useMessageSnapshot";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Message from "./Message";
 import { Conversation } from "../../typings";
 
@@ -12,11 +12,24 @@ interface Props {
 const Messages = ({ conversationId, members }: Props) => {
   const { user } = useAuth();
   const { messages } = useMessageSnapshot(conversationId, user);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="w-full lg:w-[89%] max-h-full h-screen flex flex-col justify-start space-y-2 overflow-y-auto sm:px-3 md:px-6 sticky top-0 scrollbar">
       {messages?.map((message) => (
         <Message key={message.id} {...message} members={members} />
       ))}
+      <div ref={messagesEndRef} />
     </div>
   );
 };
